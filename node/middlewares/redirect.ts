da512c564fc61c34c4a6389e1d5f69db2f63edb2
Felipe Fadul <felipe.fadul@hotmail.com>
2022-02-18 15:56:17 -0300

    fix: Input `app` in the redirect URL
    Issue: BRPA-1096

--- node/middlewares/redirect.ts
+++ node/middlewares/redirect.ts
@@ -7,7 +7,7 @@ export async function redirect(ctx: ServiceContext, next: () => Promise<void>) {
     redirectState.workspace && redirectState.workspace !== 'master'
       ? `${redirectState.workspace}--`
       : ''
-  }${redirectState.account}.myvtex.com/admin/tiktok-loading?authCode=${
+  }${redirectState.account}.myvtex.com/admin/app/tiktok-loading?authCode=${
     redirectState.authCode
   }&tradePolicyId=${redirectState.tradePolicyId}&passPhrase=${
     redirectState.passPhrase
